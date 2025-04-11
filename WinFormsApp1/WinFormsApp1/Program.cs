1144c603523416a9c5ca921163a271e9015a0ac5
KajRai <kajetanrainko@gmail.com>
2025-04-11 13:36:13 +0200

    koniec - zmiana wypisania generacji co 5 wczesniej

--- WinFormsApp1/WinFormsApp1/Program.cs
+++ WinFormsApp1/WinFormsApp1/Program.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using XorNeuralNetworkGA;
 
 namespace XorNeuralNetworkGA
 {
@@ -145,8 +146,8 @@ namespace XorNeuralNetworkGA
                 // inicjacja metody sortowania
                 SortPopulationByFitness();
 
-                // wypisanie danych co 10 - mo¿e byc za du¿o? 
-                if (generation % 10 == 0 || generation == MaxGenerations)
+                // wypisanie danych co 5
+                if (generation % 5 == 0 || generation == MaxGenerations)
                 {
                     LogGenerationInfo(generation);
                 }
@@ -166,145 +167,238 @@ namespace XorNeuralNetworkGA
             //nowy wykres jezeli tak
             chart.Invalidate();
         }
-    }
-    private void InitializePopulation() //medoda do inicjalizacji populacji
-    {
-        population = new double[PopulationSize][];
-        fitness = new double[PopulationSize];
 
-        for (int i = 0; i < PopulationSize; i++)
+        private void InitializePopulation() //medoda do inicjalizacji populacji
         {
-            population[i] = new double[WeightsCount];
-            for (int j = 0; j < WeightsCount; j++)
+            population = new double[PopulationSize][];
+            fitness = new double[PopulationSize];
+
+            for (int i = 0; i < PopulationSize; i++)
             {
-                population[i][j] = WeightMin + (WeightMax - WeightMin) * random.NextDouble();
+                population[i] = new double[WeightsCount];
+                for (int j = 0; j < WeightsCount; j++)
+                {
+                    population[i][j] = WeightMin + (WeightMax - WeightMin) * random.NextDouble();
+                }
+            }
+
+            LogMessage("Populacja zainicjowana");
+        }
+
+        private void EvaluatePopulation()
+        {
+            double totalFitness = 0;
+
+            for (int i = 0; i < PopulationSize; i++)
+            {
+                fitness[i] = CalculateFitness(population[i]);
+                totalFitness += fitness[i];
+            }
+
+            // srednia dok³adnoœæ
+            double avgFitness = totalFitness / PopulationSize;
+            avgFitnessList.Add(avgFitness);
+
+            // test dok³adnoœci
+            double bestFitness = fitness.Min();
+            bestFitnessList.Add(bestFitness);
+        }
+
+        private double CalculateFitness(double[] weights)
+        {
+            double sumSquaredError = 0;
+
+            // wynik dla 4 xorów
+            for (int sample = 0; sample < 4; sample++)
+            {
+                double input1 = xorInputs[sample * 2];
+                double input2 = xorInputs[sample * 2 + 1];
+                double target = xorTargets[sample];
+
+                double output = FeedForward(input1, input2, weights);
+                double error = target - output;
+                sumSquaredError += error * error;
+            }
+
+            return sumSquaredError;
+        }
+
+        private double FeedForward(double input1, double input2, double[] weights)
+        {
+            // ukryte neurony do sigmoidow
+            double h1 = Sigmoid(input1 * weights[0] + input2 * weights[1] + weights[2]); // peirwszy ukryty neuron
+            double h2 = Sigmoid(input1 * weights[3] + input2 * weights[4] + weights[5]); // drugi neuron ukryty
+
+            // Hidden to output layer
+            double output = Sigmoid(h1 * weights[6] + h2 * weights[7] + weights[8]); // wyjscie neuronow
+
+            return output;
+        }
+
+        private double Sigmoid(double x) // sigmoidy
+        {
+            return 1.0 / (1.0 + Math.Exp(-x));
+        }
+
+        private void SortPopulationByFitness()
+        {
+            // sortowanie po najlepszym dostosowaniu
+            Array.Sort(fitness, population);
+        }
+
+        private double[] TournamentSelection()
+        {
+
+            int[] tournamentIndices = new int[TournamentSize];
+            for (int i = 0; i < TournamentSize; i++)
+            {
+                tournamentIndices[i] = random.Next(PopulationSize);
+            }
+
+            // najlepsze jednostki do turnieju
+            int bestIndex = tournamentIndices[0];
+            for (int i = 1; i < TournamentSize; i++)
+            {
+                if (fitness[tournamentIndices[i]] < fitness[bestIndex])
+                {
+                    bestIndex = tournamentIndices[i];
+                }
+            }
+
+            return (double[])population[bestIndex].Clone();
+        }
+
+        private double[] Crossover(double[] parent1, double[] parent2)
+        {
+            double[] child = new double[WeightsCount];
+
+            // krzyzowanie 
+            int crossoverPoint = random.Next(WeightsCount);
+
+            for (int i = 0; i < WeightsCount; i++)
+            {
+                if (i < crossoverPoint)
+                {
+                    child[i] = parent1[i];
+                }
+                else
+                {
+                    child[i] = parent2[i];
+                }
+            }
+
+            return child;
+        }
+
+        private void Mutate(double[] individual)
+        {
+            for (int i = 0; i < WeightsCount; i++)
+            {
+                if (random.NextDouble() < MutationRate)
+                {
+                    // dodanie ma³ej wartosci do mutacji
+                    individual[i] += (WeightMax - WeightMin) * (random.NextDouble() * 2 - 1) * 0.1;
+
+                    // kontrola na wypadek za duzej wartosci 
+                    individual[i] = Math.Max(WeightMin, Math.Min(WeightMax, individual[i]));
+                }
             }
         }
 
-        LogMessage("Populacja zainicjowana");
-    }
-
-    private void EvaluatePopulation()
-    {
-        double totalFitness = 0;
-
-        for (int i = 0; i < PopulationSize; i++)
+        private void LogGenerationInfo(int generation)
         {
-            fitness[i] = CalculateFitness(population[i]);
-            totalFitness += fitness[i];
+            LogMessage($"Generacja: {generation}: Najlepsza dokladnoœæ = {fitness[0]:F6}, Œrednia dok³adnoœæ = {avgFitnessList.Last():F6}");
         }
-
-        // srednia dok³adnoœæ
-        double avgFitness = totalFitness / PopulationSize;
-        avgFitnessList.Add(avgFitness);
-
-        // test dok³adnoœci
-        double bestFitness = fitness.Min();
-        bestFitnessList.Add(bestFitness);
-    }
-
-    private double CalculateFitness(double[] weights)
-    {
-        double sumSquaredError = 0;
-
-        // wynik dla 4 xorów
-        for (int sample = 0; sample < 4; sample++)
+        private void TestSolution(double[] solution)
         {
-            double input1 = xorInputs[sample * 2];
-            double input2 = xorInputs[sample * 2 + 1];
-            double target = xorTargets[sample];
+            LogMessage("\r\nTestowanie najlepszej wersji:");
+            LogMessage("Bramka1\tBramka2\tCel\tWyjscie\tBlad");
 
-            double output = FeedForward(input1, input2, weights);
-            double error = target - output;
-            sumSquaredError += error * error;
-        }
-
-        return sumSquaredError;
-    }
-
-private double FeedForward(double input1, double input2, double[] weights)
-{
-    // ukryte neurony do sigmoidow
-    double h1 = Sigmoid(input1 * weights[0] + input2 * weights[1] + weights[2]); // peirwszy ukryty neuron
-    double h2 = Sigmoid(input1 * weights[3] + input2 * weights[4] + weights[5]); // drugi neuron ukryty
-
-    // Hidden to output layer
-    double output = Sigmoid(h1 * weights[6] + h2 * weights[7] + weights[8]); // wyjscie neuronow
-
-    return output;
-}
-
-private double Sigmoid(double x) // sigmoidy
-{
-    return 1.0 / (1.0 + Math.Exp(-x));
-}
-
-private void SortPopulationByFitness()
-{
-    // sortowanie po najlepszym dostosowaniu
-    Array.Sort(fitness, population);
-}
-
-private double[] TournamentSelection()
-{
-   
-    int[] tournamentIndices = new int[TournamentSize];
-    for (int i = 0; i < TournamentSize; i++)
-    {
-        tournamentIndices[i] = random.Next(PopulationSize);
-    }
-
-    // najlepsze jednostki do turnieju
-    int bestIndex = tournamentIndices[0];
-    for (int i = 1; i < TournamentSize; i++)
-    {
-        if (fitness[tournamentIndices[i]] < fitness[bestIndex])
-        {
-            bestIndex = tournamentIndices[i];
-        }
-    }
-
-    return (double[])population[bestIndex].Clone();
-}
-
-    private double[] Crossover(double[] parent1, double[] parent2)
-    {
-        double[] child = new double[WeightsCount];
-
-        // krzyzowanie 
-        int crossoverPoint = random.Next(WeightsCount);
-
-        for (int i = 0; i < WeightsCount; i++)
-        {
-            if (i < crossoverPoint)
+            for (int sample = 0; sample < 4; sample++)
             {
-                child[i] = parent1[i];
+                double input1 = xorInputs[sample * 2];
+                double input2 = xorInputs[sample * 2 + 1];
+                double target = xorTargets[sample];
+
+                double output = FeedForward(input1, input2, solution);
+                double error = target - output;
+
+                LogMessage($"{input1}\t{input2}\t{target}\t{output:F6}\t{error:F6}");
             }
-            else
+
+            LogMessage("\r\nWagi najlepszego rozwiazania:");
+            for (int i = 0; i < WeightsCount; i++)
             {
-                child[i] = parent2[i];
+                LogMessage($"Wagi {i + 1}: {solution[i]:F6}");
             }
         }
 
-        return child;
-    }
-
-    private void Mutate(double[] individual)
-    {
-        for (int i = 0; i < WeightsCount; i++)
+        private void LogMessage(string message)
         {
-            if (random.NextDouble() < MutationRate)
-            {
-                // dodanie ma³ej wartosci do mutacji
-                individual[i] += (WeightMax - WeightMin) * (random.NextDouble() * 2 - 1) * 0.1;
+            txtOutput.AppendText(message + "\r\n");
+        }
 
-                // kontrola na wypadek za duzej wartosci 
-                individual[i] = Math.Max(WeightMin, Math.Min(WeightMax, individual[i]));
+        private void chart_Paint(object sender, PaintEventArgs e)
+        {
+            if (bestFitnessList.Count == 0)
+                return;
+
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            // Wyrysowanie osi 
+            Pen axisPen = new Pen(Color.Black, 1);
+            g.DrawLine(axisPen, 30, 10, 30, 230);  // os y
+            g.DrawLine(axisPen, 30, 230, 430, 230); // os x
+
+            // Etykierty
+            g.DrawString("Dokladnosc", DefaultFont, Brushes.Black, 5, 5);
+            g.DrawString("Generacja", DefaultFont, Brushes.Black, 200, 235);
+
+            // Skala
+            int dataPoints = bestFitnessList.Count;
+            double maxFitness = Math.Max(1.0, avgFitnessList.Max());
+
+            // Punkty najlepsze
+            Pen bestPen = new Pen(Color.Blue, 1);
+            for (int i = 1; i < dataPoints; i++)
+            {
+                int x1 = 30 + (i - 1) * 400 / dataPoints;
+                int y1 = 230 - (int)(bestFitnessList[i - 1] * 220 / maxFitness);
+                int x2 = 30 + i * 400 / dataPoints;
+                int y2 = 230 - (int)(bestFitnessList[i] * 220 / maxFitness);
+
+                g.DrawLine(bestPen, x1, y1, x2, y2);
             }
+
+            //punkty srednie
+            Pen avgPen = new Pen(Color.Red, 1);
+            for (int i = 1; i < dataPoints; i++)
+            {
+                int x1 = 30 + (i - 1) * 400 / dataPoints;
+                int y1 = 230 - (int)(avgFitnessList[i - 1] * 220 / maxFitness);
+                int x2 = 30 + i * 400 / dataPoints;
+                int y2 = 230 - (int)(avgFitnessList[i] * 220 / maxFitness);
+
+                g.DrawLine(avgPen, x1, y1, x2, y2);
+            }
+
+            // Legenda
+            g.DrawLine(bestPen, 350, 20, 380, 20);
+            g.DrawString("Najlepsze", DefaultFont, Brushes.Blue, 385, 15);
+            g.DrawLine(avgPen, 350, 40, 380, 40);
+            g.DrawString("Srednie", DefaultFont, Brushes.Red, 385, 35);
         }
     }
 
-    private void LogGenerationInfo(int generation)
+    static class Program
     {
-        LogMessage($"Generacja: {generation}: Najlepsza dokladnoœæ = {fitness[0]:F6}, Œrednia dok³adnoœæ = {avgFitnessList.Last():F6}");
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
     }
+}
